ddb7877f11247b4e043439d2c807709c86009239
Leo1079 <leo1073@outlook.es>
2026-04-09 00:29:19 +0200

    Ajustamos cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
