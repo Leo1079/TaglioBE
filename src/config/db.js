a993959d55c1dd035814b10e085a9c8adfead60b
Leo1079 <leo1073@outlook.es>
2026-05-05 10:48:21 -0300

    Configuramos ssl

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,11 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    minVersion: "TLSv1.2"
+  }
+
 });
 
 module.exports = pool;
